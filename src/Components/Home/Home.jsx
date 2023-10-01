f1f785ad966dc7900f7fb1d6dc8189ec3a132ec6
omarjafor <omarjaforchy@gmail.com>
2023-10-01 20:33:04 +0600

    react spring flip card added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div>
             <h1 className="text-2xl font-bold">This is Home</h1>
-            
+            <p>Click on the image to Flip</p>
             <div className="mt-40">
                 <FlipCard></FlipCard>
             </div>
