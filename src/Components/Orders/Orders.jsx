318506309e5c8b03e9692f116a2c9fee82035077
omarjafor <omarjaforchy@gmail.com>
2023-10-01 10:52:21 +0600

    private route intruducing

---
+++ src/Components/Orders/Orders.jsx
@@ -0,0 +1,15 @@
+const Orders = () => {
+    return (
+        <div>
+            <ol>
+                <li>This is the items 1</li>
+                <li>This is the items 2</li>
+                <li>This is the items 3</li>
+                <li>This is the items 4</li>
+                <li>This is the items 5</li>
+            </ol>
+        </div>
+    );
+};
+
+export default Orders;
---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">This is Your Profile</h1>
+        </div>
+    );
+};
+
+export default Profile;
