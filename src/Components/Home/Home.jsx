40a282068ee4be664754ee17990fa91ff7f56239
omarjafor <omarjaforchy@gmail.com>
2023-10-02 22:54:12 +0600

    update with practice

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div>
             <h1 className="text-2xl font-bold">This is Home</h1>
-            <p> Click On The Image to Flip </p>
+            <p> Click on The Image to Flip </p>
             <div className="mt-60">
                 <FlipCard></FlipCard>
             </div>
